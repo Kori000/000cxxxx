866bc4cfbd32cc8569003a819f67d544b4219aa8
Kori <1723895562@qq.com>
2022-11-03 11:16:01 +0800

    修改分割线组件

--- src/components/CutLine.vue
+++ src/components/CutLine.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-32">
     <div class=" flex justify-center">
-      <img src="../assets/cut-line.png" class="">
+      <img src="../assets/cut-line.png" class="sm:max-w-full max-w-[70vw]">
     </div>
   </div>
 </template>
